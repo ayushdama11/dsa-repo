561e6fed3b0988ee09ae0648bd88e6873bade3fc
Ayush Dama <ayushdama07@gmail.com>
2024-10-25 19:33:29 +0530

    dll push

--- 39_LinkedList/7_Linkedlist3.cpp
+++ 39_LinkedList/7_Linkedlist3.cpp
@@ -21,7 +21,7 @@
 // };
 
 
-// ** Leetcode 61 - Rotate List
+// ** Leetcode 61 - Rotate List 
 
 // class Solution {
 // public:
@@ -36,6 +36,7 @@
 //             temp = temp -> next;
 //             n++;
 //         }
+
 //         k = k % n;
 //         if(k == 0) return head;
 
@@ -51,6 +52,7 @@
 //         temp -> next = NULL;
 
 //         return head;
+
 //     }
 // };
 
@@ -60,6 +62,7 @@
 // class Solution {
 // public:
 //     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+//      // creting a vector with m rows and n columns
 //         vector< vector<int> > arr(m,vector<int> (n,-1));
 //         int minr = 0;
 //         int maxr = m-1;
@@ -67,7 +70,8 @@
 //         int maxc = n-1;
 //         ListNode* temp = head;
 
-//         while(minr<=maxr && minc<=maxc) {
+//         while(minr<=maxr && minc<=maxc) { 
+
 //             // right
 //             for(int j=minc; j<=maxc; j++) {
 //                 if(temp==NULL) return arr;
@@ -124,7 +128,7 @@
 //         ListNode* c = new ListNode(-1);
 //         ListNode* tempC = c;
 
-//         while(tempA!=NULL && tempB!=NULL) {
+//         while(tempA!=NULL && tempB!=NULL) { 
 //             if(tempA->val <= tempB->val) {  // c wali ll me a ke node ko add kro
 //                 ListNode* t = new ListNode(tempA->val);
 //                 tempC->next = t;
@@ -247,7 +251,6 @@
 //     ListNode* mergeKLists(vector<ListNode*>& arr) {
 //         // empty ho to return null
 //         if(arr.size()==0) return NULL;
-
 //         while(arr.size()>1) {
 //             // starting k 2 ele pop back krke merge kro 
 //             ListNode* a = arr[0];    
@@ -318,3 +321,220 @@
 
 
 // ** Leetcode 86 - Partition List 
+
+// class Solution {
+// public:
+//     ListNode* partition(ListNode* head, int x) {
+//         ListNode* lo = new ListNode(100);
+//         ListNode* hi = new ListNode(100);
+//         ListNode* tempLo = lo;  // pointer pointing to lo
+//         ListNode* tempHi = hi;  // pointer poitning to hi
+//         ListNode* temp = head;  // pointer pointing to head
+//         while (temp!=NULL) {
+//             if(temp->val < x) {
+//                 tempLo->next = temp;
+//                 temp = temp->next;
+//                 tempLo = tempLo->next;
+//             }
+//             else {  // temp->val > x
+//                 tempHi->next = temp;
+//                 temp = temp->next;
+//                 tempHi = tempHi->next;
+//             }
+//         }
+
+//         tempLo->next = hi->next;
+//         tempHi->next = NULL;    // last node 
+
+//         return lo->next;
+//     }
+// };
+
+
+// ** Odd Even Linked List - Leetcode 328
+
+// class Solution {
+// public:
+//     ListNode* oddEvenList(ListNode* head) {
+//         if(!head || !head->next) return head;
+
+//         ListNode* odd = head;
+//         ListNode* even = head->next;
+//         ListNode* evenHead = even;
+
+//         while(even && even->next) {
+//             odd->next = even->next;
+//             odd = odd->next;    
+//             even->next = odd->next;
+//             even = even->next;
+//         }
+
+//         odd->next = evenHead;
+//         return head;
+//     }
+// };
+
+
+// ** Reverse Linked list - leetcode 206
+
+// ** Method 1 - Iterative
+// class Solution {    // O(N^2)
+// public:
+//     ListNode* getNodeAt(ListNode* head, int idx) {
+//         ListNode* temp = head;
+//         // 1 2 3 4 5 
+//         for(int i = 1; i<=idx; i++) {
+//             temp = temp->next;
+//         }
+//         return temp;
+//     }
+//     ListNode* reverseList(ListNode* head) {
+//         int n = 0;
+//         ListNode* temp = head;
+//         while(temp) {
+//             temp = temp->next;
+//             n++;
+//         }
+//         int i = 0;
+//         int j = n-1;
+//         while(i<j) {
+//             ListNode* left = getNodeAt(head,i);
+//             ListNode* right = getNodeAt(head,j);
+//             int t = left->val;
+//             left->val = right->val;
+//             right->val  = t;
+//             i++;
+//             j--;
+//         }
+//         return head;
+//     }
+// };
+
+// ** Method 2 - iterative
+
+// class Solution {     // tc - O(N) , sc - O(1)
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+//         ListNode* Next = head;
+
+//         while(curr) {
+//             Next = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = Next;
+//         }
+
+//         return prev;
+//     }
+// };
+
+// ** Method 3 - recursive
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         // base case - if size is 0 or 1
+//         if(head==NULL || head->next==NULL) return head;
+
+//         // head ke next se end tak sabko recursively reverse krdoo
+//         ListNode* newHead = reverseList(head->next);
+
+//         // iteratively reversing first and second node 
+//         head->next->next = head;
+//         head->next = NULL;
+//         return newHead;
+//     }
+// };
+
+
+
+// **** Leetcode 234 - Palindrome linked list
+
+// ** Method 1 : ( tc - O(N) , sc - O(N) )
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode *prev = NULL, *curr = head, *Next = head;
+//         while(curr) {
+//             Next = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = Next;
+//         }
+//         return prev;
+//     }
+//     bool isPalindrome(ListNode* head) {
+//         ListNode* c = new ListNode(100);
+//         // deep copy of head
+//         ListNode* temp = head;
+//         ListNode* tempC = c;
+//         while(temp) {
+//             ListNode* node = new ListNode(temp->val);
+//             tempC->next = node;
+//             temp = temp->next;
+//             tempC = tempC->next;
+//         }
+//         c = c->next;
+
+//         // now reverse the c wali ll
+//         c = reverseList(c);
+
+//         // checking if the reversed list is same as the original one :
+//         ListNode* a = head;
+//         ListNode* b = c;
+//         while(a) {
+//             if(a->val != b->val) return false;
+//             a = a->next;
+//             b = b->next;
+//         }
+//         return true;
+//     }
+// };
+
+// ** Method 2 : ( tc - O(N) , sc - O(1) )
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {     // O(N) - tc
+//         ListNode *prev = NULL, *curr = head, *Next = head;
+//         while(curr) {
+//             Next = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = Next;
+//         }
+//         return prev;
+//     }
+//     bool isPalindrome(ListNode* head) {
+//         // if the first half and second half are reverse of each other than it is palindrome
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         // finding the "left middle" 
+//         while(fast->next!=NULL && fast->next->next!=NULL) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+
+//         // slow is at the left middle
+//         ListNode* newHead = reverseList(slow->next);
+
+//         // ab traverse kro and check in both the parts
+//         ListNode* a = head;
+//         ListNode* b = newHead;
+//         while(b) {
+//             if(a->val != b->val) return false;
+//             a = a->next;
+//             b = b->next;
+//         }
+
+//         return true;
+//     }
+// }; 
+
+
+
+// ** Leetcode 92 - Reverse Linked List
+
+// ** Leetcode 143 - Reorder List
+
